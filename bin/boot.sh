9c372022162c80a3e5b9dcdc1723d02373a098d0
Rémi Prévost <remi@exomel.com>
2015-08-31 08:37:41 -0400

    Build app on boot

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cd $HOME/app
+build_env=${EMBER_ENV:-production}
+node_modules/ember-cli/bin/ember build --environment $build_env | indent
+cd $HOME
+
 ruby $HOME/config/htpasswd.rb
 erb $HOME/config/nginx.conf.erb > $HOME/config/nginx.conf
 
